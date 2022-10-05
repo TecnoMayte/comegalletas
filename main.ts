b36f38fa5761e14a73c118ae57a75c3a9e0e175a
TecnoMayte <114059096+TecnoMayte@users.noreply.github.com>
2022-10-05 19:51:09 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,6 +8,13 @@ function comegalletas () {
         coco.change(LedSpriteProperty.Y, 1)
     }
 }
+function comegalletas2 () {
+    coco = game.createSprite(randint(0, 4), 0)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(500)
+        coco.change(LedSpriteProperty.Y, 1)
+    }
+}
 input.onButtonPressed(Button.B, function () {
     galleta.move(1)
 })
@@ -17,6 +24,17 @@ galleta = game.createSprite(2, 4)
 comegalletas()
 game.setScore(0)
 basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        if (coco.isTouching(galleta)) {
+            basic.showIcon(IconNames.Sad)
+            game.gameOver()
+        }
+        if (coco.isTouchingEdge()) {
+            coco.delete()
+            game.addScore(1)
+            comegalletas()
+        }
+    }
     if (coco.isTouching(galleta)) {
         basic.showIcon(IconNames.Sad)
         game.gameOver()
@@ -25,5 +43,6 @@ basic.forever(function () {
         coco.delete()
         game.addScore(1)
         comegalletas()
+        comegalletas2()
     }
 })
