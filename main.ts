22a3e62d246afc6b672fb70d6b5162105d64a2cf
TecnoMayte <114059096+TecnoMayte@users.noreply.github.com>
2022-10-05 19:33:18 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,29 @@
+input.onButtonPressed(Button.A, function () {
+    galleta.move(-1)
+})
+function comegalletas () {
+    coco = game.createSprite(randint(0, 4), 0)
+    for (let index = 0; index < 4; index++) {
+        basic.pause(500)
+        coco.change(LedSpriteProperty.Y, 1)
+    }
+}
+input.onButtonPressed(Button.B, function () {
+    galleta.move(1)
+})
+let coco: game.LedSprite = null
+let galleta: game.LedSprite = null
+galleta = game.createSprite(2, 4)
+comegalletas()
+game.setScore(0)
+basic.forever(function () {
+    if (coco.isTouching(galleta)) {
+        basic.showIcon(IconNames.Sad)
+        game.gameOver()
+    }
+    if (coco.isTouchingEdge()) {
+        coco.delete()
+        game.addScore(1)
+        comegalletas()
+    }
+})
